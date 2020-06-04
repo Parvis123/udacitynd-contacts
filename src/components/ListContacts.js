bf5635b6de6c636a3cbcd383622c64c018f441ba
Michael <michael_parvis@hotmail.com>
2020-06-04 20:30:37 +0100

    added filter, reset filter

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -17,22 +17,42 @@ class ListContacts extends Component {
     }))
   }
 
+  clearQuery = () =>{
+    this.updateQuery('');
+  }
+
   render() {
+    const { query } = this.state;
+    const { contacts, onDeleteContact } = this.props;
+
+    const showingContacts = query === '' 
+    ? contacts:
+    contacts.filter((c) => (
+      c.name.toLowerCase().includes(query.toLowerCase())
+    ))
+
     return (
       <div className='list-contacts'>
-        {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
             placeholder='Search For Contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
            />
-          
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+              <span>Now showing {showingContacts.length} of {contacts.length} contacts.</span>
+              <br />
+              <button onClick={this.clearQuery}>Show all contacts</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div
                 className='contact-avatar'
@@ -46,7 +66,7 @@ class ListContacts extends Component {
                 <p>{contact.handle}</p>
               </div>
               <button
-              onClick={() => this.props.onDeleteContact(contact)}
+              onClick={() => onDeleteContact(contact)}
               className='contact-remove'>
                 Remove
             </button>
